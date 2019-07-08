786e158abb36be45b9c79e68d961c124f957f9f0
Yoya <unrealboundary@gmail.com>
2019-07-08 21:39:35 +0800

    JAVA 8

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -12,34 +12,46 @@ import vo.User;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
     private static List<User> list = new ArrayList<>();
-    
+
     // http://localhost:8080/SpringMVC_Web/mvc/user/
     @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
     public String get() {
-        return "get";
+        return "get:" + list;
     }
-    
+
     // http://localhost:8080/SpringMVC_Web/mvc/user/
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @ResponseBody
     public String post(User user) {
+        list.add(user);
         return "post:" + user;
     }
-    
+
     // http://localhost:8080/SpringMVC_Web/mvc/user/
     @RequestMapping(value = "/", method = RequestMethod.PUT)
     @ResponseBody
     public String put(User user) {
+        int id = user.getNum().getId();
+        list.stream()
+                .filter(u -> u.getNum().getId() == id)
+                .forEach(u -> {
+                    u.setUname(user.getUname());
+                    u.setUage(user.getUage());
+                });
         return "put:" + user;
     }
-    
+
     // http://localhost:8080/SpringMVC_Web/mvc/user/
     @RequestMapping(value = "/", method = RequestMethod.DELETE)
     @ResponseBody
     public String delete(User user) {
+        int id = user.getNum().getId();
+        User del_user = list.stream().filter(u -> u.getNum().getId() == id).findFirst().get();
+        list.remove(del_user);
         return "delete:" + user;
     }
-    
+
 }
